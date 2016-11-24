94fd7f29331203a75658c2ec7a9f9721d7c5e359
NiketaJain <niketa.jain08@gmail.com>
2016-11-24 17:47:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,9 +5,9 @@ app.controller('myCtrl',function($scope)
 {
 	
 	var tempEdit=0;
-	
+	$scope.listdata={};
 	$scope.Question_list=[];
-	$scope.answerType='radio';
+	$scope.answerType='SinglePunch';
 	$scope.ishidden=false;
 	$scope.products=[];
 	$scope.correctResult=[];
@@ -18,7 +18,9 @@ app.controller('myCtrl',function($scope)
 	$scope.error1=false;
 	$scope.selectItem=$scope.products[0];
 	$scope.selected1=undefined;
-
+	$scope.createbutton=true;
+	$scope.updatebutton=true;
+	$scope.cancelcreate=true;
 	
 	
 /****************Array-Of-Object(JSON)********************/
@@ -29,12 +31,23 @@ app.controller('myCtrl',function($scope)
 		data.Question=$scope.ques;
 		data.Answer_Type=$scope.answerType;
 		data.Answer_key=$scope.products;
+		data.resultKey=$scope.correctResult;
 		data.Answer_Selected_Checkbox=$scope.storeResult();
 		data.Answer_Selected_Radio=$scope.selectItem;
 		$scope.Question_list.push(data);
+		if($scope.products.length==1)
+		{
+			$scope.error1=true;
+			$scope.ErrorText1="Add 2 or more than 2 answer ";
+		}
+		else
+		{
+				$scope.error1=false;
+		}
 		$scope.Queslist=true;
 		$scope.counter=$scope.counter+1;
 		$scope.qid=$scope.counter;
+
 		$scope.Reset_Form();
 		$scope.ishidden=false;
 
@@ -45,14 +58,15 @@ app.controller('myCtrl',function($scope)
 $scope.Reset_Form=function()
 {
 	$scope.ques="";
-	$scope.answerType='radio';
+	$scope.answerType='SinglePunch';
 	$scope.answer="";
 	$scope.products=[];
 	$scope.Queslist=true;
-	angular.forEach($scope.correctResult,function(ans,ind){
-				ans.res=false;
-			});
-
+	$scope.correctResult=[];
+	$scope.result=[];
+	$scope.ishidden=false;
+	$scope.showtype=false;
+	
 } 
 	
 /***************Add-Answer*****************************/
@@ -127,6 +141,7 @@ $scope.Reset_Form=function()
 					$scope.errorShow=true;
 					$scope.errorText="Answer is already in the list1";
 			}
+
 				
 			$scope.answer="";
 		
@@ -151,12 +166,16 @@ $scope.answerShow=function(index)
 
 /************Cancel-button********************/
 
-	$scope.cancel=function()
+	$scope.cancelList=function()
 	{
 		$scope.addAnswerHide=false;
 		$scope.editAnswershow=false;
 		$scope.answer="";
-		ishidden=false;
+		//ishidden=false;
+		$scope.createbutton=true;
+		$scope.updatebutton=true;
+		$scope.qid=$scope.counter;
+		$scope.Reset_Form();
 	
 	}
 
@@ -164,18 +183,20 @@ $scope.answerShow=function(index)
 
 	$scope.result1=function(value)
 	{
-		if(value=="checkbox")
+		if(value=="MultiPunch")
 		{
 			$scope.showtype=true;
+
 			$scope.selectItem="";
 
 		}
-		if(value=="radio")
+		if(value=="SinglePunch")
 		{
 			$scope.showtype=false;
 			angular.forEach($scope.correctResult,function(ans,ind){
 				ans.res=false;
 			});
+
 		}
 	}
 
@@ -215,7 +236,67 @@ $scope.DeleteQList=function(x)
 	$scope.Question_list.splice(x,1);
 }
 
+/******************Edit-Question-List********************/
+
+$scope.EditQList=function(x)
+{
+	$scope.createbutton=false;
+	$scope.updatebutton=false;
+	$scope.cancelcreate=false;
+	$scope.listdata=$scope.Question_list[x];
+	$scope.qid=$scope.Question_list[x].Question_Id;
+	$scope.ques=$scope.Question_list[x].Question;
+	$scope.answerType=$scope.Question_list[x].Answer_Type;
+	$scope.products=angular.copy($scope.Question_list[x].Answer_key);
+	if($scope.Question_list[x].Answer_Selected_Radio==="")
+	{
+		angular.forEach($scope.Question_list[x].resultKey,function(ans,ind)
+		{
+			$scope.correctResult[ind].res=ans.res;
+		});
+		
+		$scope.selectItem="";
+		$scope.showtype=true;
+	}
+	if($scope.Question_list[x].Answer_Type==="SinglePunch" && $scope.Question_list[x].Answer_Selected_Checkbox.length===0){
+		
+		$scope.selectItem=$scope.Question_list[x].Answer_Selected_Radio;
+		$scope.showtype=false;
+	
+	}
+	
+	
+}
+
+/************Update-Question-List************************/
+
+
+$scope.updateList=function(Objid)
+{
+	if(Objid===$scope.listdata.Question_Id)
+	{
+		
+		$scope.listdata.Question_Id=$scope.qid;
+		$scope.listdata.Question=$scope.ques;
+		$scope.listdata.Answer_Type=$scope.answerType;
+		$scope.listdata.Answer_key=$scope.products;
+		$scope.listdata.Answer_Selected_Checkbox=$scope.storeResult();
+		$scope.listdata.Answer_Selected_Radio=$scope.selectItem;
+
+	}
+	if($scope.products.length==1)
+		{
+			$scope.error1=true;
+			$scope.ErrorText1="Add 2 or more than 2 answer ";
+		}
+		else
+		{
+				$scope.error1=false;
+		}
+}
 
 });
+
+
 
  
